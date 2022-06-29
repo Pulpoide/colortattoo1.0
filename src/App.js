1d4ac6d4aa292f858484c3f10c2d280dbba48b23
Joaquin Olivero <joacolivero@gmail.com>
2022-06-29 17:50:36 -0300

    First Commit ♦

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import img_logo from './imgs/Logo_apaizado.jpg';
+import ImgGalery from './components/ImgGalery';
+import Bar from './components/Bar';
+/* css */
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container bg-dark">
+        <Bar/>
+        <div className="container bg-dark">
+        {<img src={img_logo} className="img-fluid"/>}
+         <ImgGalery/> 
+        <div className='container-down' />
+      </div>
     </div>
   );
 }
